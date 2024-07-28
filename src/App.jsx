8aef4bb804d32ad2fa1749b4c1f1aae7ca1a4516
mohamuhsin <muhsinohoho@outlook.com>
2024-07-29 00:11:11 +0300

    managed a state to handle our answers and derived our next questions from the same state, we mapped through our questions file conditionally and outputted everything as JSX elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
     return (
         <>
             <Header />
+            <main>
+                <Quiz />
+            </main>
         </>
     );
 }
