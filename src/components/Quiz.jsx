6efa82fd447c6992a78f7b1df08536c16c0ffce5
mohamuhsin <muhsinohoho@outlook.com>
2024-07-29 03:43:13 +0300

    we shuffled our answers to change using the sort() method, it takes two elements e.g (a,b) it edits an array when a function returns negative number and keeps the array unchanged whne the function returns positive number

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,6 +6,8 @@ export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswer) => {
@@ -18,7 +20,7 @@ export default function Quiz() {
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>
                                 {answer}
