0fbe4d83a724180a2a41e2f427656b53e2c7bb03
mohamuhsin <muhsinohoho@outlook.com>
2024-07-31 16:26:36 +0300

    the dependencies as functions was causing our setTimeout to keep executing, so we introduced useCallbcak hook to keep the functions from being recreated everytiome jsx in Quiz.jsx executes

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,10 +4,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
+        console.log("SET TIMEOUT");
         setTimeout(onTimeout, timeout);
     }, [timeout, onTimeout]);
 
     useEffect(() => {
+        console.log("SET INTERVAL");
         setInterval(() => {
             setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
         }, 100);
