da70ffbce6662912fea221c07be8766669661ca8
mohamuhsin <muhsinohoho@outlook.com>
2024-08-02 20:27:49 +0300

    we have set different timers based on selectedAnswer

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
@@ -23,5 +23,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
         };
     }, []);
 
-    return <progress value={remainingTime} id="question-time" max={timeout} />;
+    return (
+        <progress
+            value={remainingTime}
+            id="question-time"
+            max={timeout}
+            className={mode}
+        />
+    );
 }
